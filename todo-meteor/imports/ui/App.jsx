1b90ead738e7409fc41adefa4b9ee0039883f32f
Athus27 <athussilva27@gmail.com>
2026-03-10 21:47:02 -0300

    add test component and task component for React testing

--- todo-meteor/imports/ui/App.jsx
+++ todo-meteor/imports/ui/App.jsx
@@ -1,13 +1,19 @@
 import { Counter } from "./Counter.jsx";
 import { Header } from "./Header.jsx";
 import { Info } from "./Info.jsx";
+// todo-meteor\temp\react-tests\TestTask.jsx
+import { TestTask } from "../../temp/react-tests/TestTask.jsx";
 
+// tudo depois do => é o que a função retorna
+
+//rafce cria componente rápido
 export const App = () => (
   <div className="page">
     <Header />
     <main className="main">
       <Counter />
       <Info />
+      <TestTask />
     </main>
   </div>
 );
